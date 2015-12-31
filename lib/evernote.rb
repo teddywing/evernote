f10ec2efe2e2984109545e496f36067cb3707dd6
Teddy Wing <git@teddywing.com>
2015-12-30 16:32:50 -0800

    lib/evernote: Remove vendored Thrift imports
    These imports caused a `LoadError` because the vendored Thrift and
    Evernote dependencies were removed in
    2ebc0504d3b5e11676bf37d5322cf48a24b3785c.
    
    Delete the imports to fix this error.

--- lib/evernote.rb
+++ lib/evernote.rb
@@ -1,12 +1,5 @@
 require 'rubygems'
 
-thrift_path = File.expand_path(File.dirname(__FILE__) + "/../vendor")
-$LOAD_PATH.unshift thrift_path
 require 'thrift'
-
-gen_rb_path = File.expand_path(File.dirname(__FILE__) + "/../vendor/gen-rb")
-$LOAD_PATH.unshift gen_rb_path
-$LOAD_PATH.unshift "#{gen_rb_path}/evernote/edam"
-require "#{gen_rb_path}/evernote"
 require "evernote/user_store"
 require "evernote/note_store"
